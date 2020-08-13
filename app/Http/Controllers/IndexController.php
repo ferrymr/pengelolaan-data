985c14de10b7a3b44aa8d54bb95733c9738e5afb
juniarsofyan <choerulsofyanmf@gmail.com>
2020-08-14 04:35:08 +0700

    Minor fix - Fix query

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -45,7 +45,7 @@ class IndexController extends Controller
                         ->join('tb_det_jual', 'tb_det_jual.kode_barang', '=', 'cn_barang.kode_barang')
                         ->select('cn_barang.kode_barang', 'cn_barang.nama', 'cn_barang.h_nomem AS harga', DB::raw('SUM(tb_det_jual.jumlah) as jumlah_jual'))
                         ->where('cn_barang.unit', 'PIECES')
-                        ->groupBy('cn_barang.kode_barang')
+                        ->groupBy('cn_barang.kode_barang', 'cn_barang.nama')
                         ->orderBy('jumlah_jual', 'desc')
                         ->limit(8)
                         ->get();
@@ -54,7 +54,7 @@ class IndexController extends Controller
                         ->join('tb_det_jual', 'tb_det_jual.kode_barang', '=', 'cn_barang.kode_barang')
                         ->select('cn_barang.kode_barang', 'cn_barang.nama', 'cn_barang.h_nomem AS harga', DB::raw('SUM(tb_det_jual.jumlah) as jumlah_jual'))
                         ->where('cn_barang.unit', 'SERIES')
-                        ->groupBy('cn_barang.kode_barang')
+                        ->groupBy('cn_barang.kode_barang', 'cn_barang.nama')
                         ->orderBy('jumlah_jual', 'desc')
                         ->limit(8)
                         ->get();
@@ -62,7 +62,7 @@ class IndexController extends Controller
         $bestSellingProducts = DB::table('cn_barang')
                         ->join('tb_det_jual', 'tb_det_jual.kode_barang', '=', 'cn_barang.kode_barang')
                         ->select('cn_barang.kode_barang', 'cn_barang.nama', 'cn_barang.h_nomem AS harga', DB::raw('SUM(tb_det_jual.jumlah) as jumlah_jual'))
-                        ->groupBy('cn_barang.kode_barang')
+                        ->groupBy('cn_barang.kode_barang', 'cn_barang.nama')
                         ->orderBy('jumlah_jual', 'desc')
                         ->limit(16)
                         ->get();
