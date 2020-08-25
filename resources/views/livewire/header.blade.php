85f41590748a775ce0a495f5a77fd6ecd611cb04
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-08-25 23:59:02 +0700

    add menu for Mobile Only

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -268,6 +268,12 @@
                     <li class="menu-item">
                         <a href="{{ route('products.category', 'series') }}" class="kt-item-title" title="About">Series</a>
                     </li>
+                    <li class="menu-item">
+                        <a href="{{ route('products.category', 'series') }}" class="kt-item-title" title="About">Series</a>
+                    </li>
+                    <li class="menu-item mobile-only" style="margin-top: 16px">
+                        <a href="{{ route('login') }}" class="kt-item-title" title="Login">Login Register</a>
+                    </li>
                 </ul>
             </div>
         </div>
