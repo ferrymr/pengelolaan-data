5f3d37ffc44fcefb344793cc991a0a7e49e9cb86
juniarsofyan <choerulsofyanmf@gmail.com>
2020-08-26 14:57:46 +0700

    Fix MyCart functionalities
    - count totalItems
    - count subtotal
    - add function refreshData

--- app/Http/Livewire/MyCart.php
+++ app/Http/Livewire/MyCart.php
@@ -8,12 +8,12 @@ use Livewire\Component;
 class MyCart extends Component
 {
     public $cartItems;
-    public $cartAmount = 0;
+    public $totalItems;
+    public $subtotal;
 
     public function mount()
     {
-        $this->cartItems = Cart::get();
-        // $this->cartAmount = 0; 
+        $this->refreshData();
     }
 
     public function render()
@@ -21,37 +21,42 @@ class MyCart extends Component
         return view('livewire.my-cart');
     }
 
-    /* public function removeFromCart($productCode)
+    public function refreshData()
+    {
+        $this->cartItems = Cart::get();
+        $this->hitungSubtotal();
+        $this->hitungTotalItems();
+    }
+
+    public function hitungSubtotal()
+    {
+        $subtotal = 0;
+
+        foreach($this->cartItems as $cartItem) {
+            $subtotal += $cartItem['subtotal'];
+        }
+
+        $this->subtotal = $subtotal;
+    }
+
+    public function hitungTotalItems()
+    {
+        $totalItems = 0;
+
+        foreach($this->cartItems as $cartItem) {
+            $totalItems += $cartItem['qty'];
+        }
+
+        $this->totalItems = $totalItems;
+    }
+
+    public function removeFromCart($productCode)
     {
         // $user_id = 1; // ambil dari session nanti mah
         // ShoppingCart::where('users_id', '=', $user_id)->where('kode_barang', '=', $product->kode_barang)->delete();
-        
+
         Cart::remove($productCode);
-        $this->emit('refreshCartItems');
-    } */
-    
-    public function incrementQty($productCode)
-    {
-        $cart = Cart::get();
-        $cart[$productCode]['qty']++;
-        Cart::set($cart);
-        $this->emit('refreshCartItems');
-    }
-
-    public function decrementQty()
-    {
-        if ($this->qty > 1) {
-            $this->qty--;
-        }
-    }
-
-    public function addToCart()
-    {
-        $this->product->qty = $this->qty;
-
-        Cart::add($this->product);
-
-        $this->resetQty();
+        $this->refreshData();
         $this->emit('refreshCartItems');
     }
 }
