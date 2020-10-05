4a70d7708ec6ba69a20462b4319e11597d24a40e
Ranu <70423148+Ranu85@users.noreply.github.com>
2020-10-05 10:04:38 +0700

    Perubahan huruf di BarangController

--- app/Http/Controllers/barangController.php
+++ app/Http/Controllers/barangController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class barangController extends Controller
+class BarangController extends Controller
 {
     public function show()
     {
