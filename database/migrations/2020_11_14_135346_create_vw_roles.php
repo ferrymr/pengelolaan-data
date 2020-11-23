bacf7af8b30ad83207805941ec06957f7de2ff65
Alan Wae <61464428+alanwez@users.noreply.github.com>
2020-11-23 14:23:25 +0700

    fix view roles

--- database/migrations/2020_11_14_135346_create_vw_roles.php
+++ database/migrations/2020_11_14_135346_create_vw_roles.php
@@ -14,6 +14,7 @@ class CreateVwRoles extends Migration
      */
     public function up()
     {
+        DB::dropIfExists('vw_roles');
         DB::statement("
             CREATE VIEW `vw_roles` AS select 
                 `users`.`id` AS `id`,
